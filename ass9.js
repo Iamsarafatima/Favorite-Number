857a0a4160efb5c203682e83f2001e1560274dfc
Iamsarafatima <162489179+Iamsarafatima@users.noreply.github.com>
2024-03-10 05:42:11 -0700

    Add files via upload

---
+++ ass9.js
@@ -0,0 +1,3 @@
+// ex:9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favorite_number = 12;
+console.log("My favorite number is", favorite_number);
---
+++ ass9.ts
@@ -0,0 +1,4 @@
+// ex:9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favorite_number: number = 12;
+console.log("My favorite number is", favorite_number);
